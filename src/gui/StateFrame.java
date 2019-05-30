999afb8b0be74ff0e103e66cc339dfb33ff20f66
czh098tom <czh098tom@163.com>
2019-05-30 20:49:34 +0800

    Add comment

--- src/gui/StateFrame.java
+++ src/gui/StateFrame.java
@@ -5,24 +5,51 @@ import java.awt.event.*;
 
 import javax.swing.JFrame;
 
+/**
+ * Base class for all {@link JFrame} that represent a state in {@link Control}.
+ * @author Zihao Chen
+ */
 public abstract class StateFrame extends JFrame implements ActionListener {
 	
 	private static final long serialVersionUID = 1L;
+    /**
+     * Store the state transition table when invoking Action event.
+     */
 	private HashMap<Object,Supplier<StateFrame>> commands=new HashMap<Object,Supplier<StateFrame>>();
-	
+
+    /**
+     * Store the parent {@link Control}.
+     */
 	private StateManager parent; 
-	
+
+    /**
+     * Store whether the JFrame is disposing manually (by clicking X) or invoked by transition.
+     */
 	private boolean isDisposingManually=true;
 	
+    /**
+     * Initialize the Frame with parent control.
+     * @param parent the parent control.
+     */
 	protected StateFrame(StateManager parent) {
 		this.parent=parent;
 		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 	}
-	
+
+    /**
+     * Register a transition.
+     * @param eventSource : The object invoking Action event.
+     * @param target : The anonymous inner class that had a function returning the transition target.
+     */
 	protected final void register(Object eventSource, Supplier<StateFrame> target) {
 		commands.put(eventSource, target);
 	}
 	
+    /**
+     * Register operation when clicking X manually.
+     * @param target : The anonymous inner class that had a function returning the transition target.
+     *               Returning null will cause the program exit.
+     */
 	protected final void registerClosing(Supplier<StateFrame> target) {
 		if(target!=null){
 			this.addWindowListener(new WindowAdapter() {
@@ -40,14 +67,21 @@ public abstract class StateFrame extends JFrame implements ActionListener {
 		}
 	}
 	
+    /**
+     * This method gets the parent control of this frame.
+     * @return An object typed {@link Control}.
+     */
 	protected final StateManager getParentManager() {
 		return parent;
 	}
 	
+    /**
+     * Deal with Action event.
+     * @param arg0 : the event argument.
+     */
 	@Override
 	public final void actionPerformed(ActionEvent arg0) {
 		isDisposingManually=false;
 		parent.setState(commands.get(arg0.getSource()).get());
 	}
-
 }
--- src/gui/StateManager.java
+++ src/gui/StateManager.java
@@ -1,8 +1,19 @@
 package gui;
 
+/**
+ * Base class for all managers.
+ * @author Zihao Chen
+ */
 public abstract class StateManager {
+    /**
+     * Store the only frame that manager takes.
+     */
 	private StateFrame current;
-	
+
+    /**
+     * Set the frame the control takes, then show it.
+     * @param target : The target frame.
+     */
 	public void setState(StateFrame target) {
 		if(target!=current) {
 			if(current!=null)
